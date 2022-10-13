294c6a9d609bac60e289d79838e8aee8d367881c
jpho8799 <josephine.pho8799@gmail.com>
2022-10-12 23:42:51 -0400

    added leaflet

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -6,26 +7,35 @@ module.exports = {
     entry: {
         index: './src/index.js'
     },
+    devServer: {
+        static: './dist',
+    },
+
     output: {
         filename: '[name][contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
-
     },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            
+        ],
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Restaurant Roulette',
+        new HtmlWebpackPlugin ({
+            title: 'To Do List',
             filename: 'index.html',
             template: 'src/index.html'
         })
     ]
 
-} 
+
+};
