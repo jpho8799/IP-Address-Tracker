d37aa0634d68d0d7f9255f08103181d5122b6e7b
jpho8799 <josephine.pho8799@gmail.com>
2022-10-19 02:00:25 -0400

    display api information on displaycard

--- src/modules/request/api.js
+++ src/modules/request/api.js
@@ -2,15 +2,16 @@
 
 async function apiCall(ipAddress){
     try{
-        console.log(ipAddress);
+        console.log('reached');
         const API_KEY = process.env.IP_API;
-        console.log(API_KEY);
         let URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAddress}`;
 
         let response = await fetch(URL, {
-         mode: 'cors'
+         mode: 'cors',
         })
-        return response.json();
+
+        return await response.json();
+        
 
     }catch(error){
         console.log('GEOIP💀',error)
